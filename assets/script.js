f7d248bc0c2e6754db8d5d54782cb24fe9a08d68
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-06-05 15:51:22 -0500

    added var workDay to add times for the day

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,46 @@ function headerDate() {
     $("#currentDay").text(currentDate);
 }
 headerDate();
+
+// var for all the times of the work day
+
+var workDay = [
+    {
+        id: "0",
+        hour: "9AM",
+        time: "9",
+    },    {
+        id: "1",
+        hour: "10AM",
+        time: "10",
+    },    {
+        id: "2",
+        hour: "11AM",
+        time: "11",
+    },    {
+        id: "3",
+        hour: "12PM",
+        time: "12",
+    },   {
+        id: "4",
+        hour: "1PM",
+        time: "13",
+    },    {
+        id: "5",
+        hour: "2PM",
+        time: "14",
+    },    {
+        id: "06",
+        hour: "3PM",
+        time: "15",
+    },    {
+        id: "07",
+        hour: "4PM",
+        time: "16",
+    },    {
+        id: "08",
+        hour: "5PM",
+        time: "17",
+    },
+];
+
